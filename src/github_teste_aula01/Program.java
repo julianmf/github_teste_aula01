e8278e9fb0f9a68dc195ac6ce258a6a0a8d64ee9
julianmf <figueredo.juliano@hotmail.com>
2020-04-24 18:36:20 -0300

    Projeto criado

---
+++ src/github_teste_aula01/Program.java
@@ -0,0 +1,12 @@
+package github_teste_aula01;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		System.out.println("criação de projeto github");
+
+	}
+
+}
